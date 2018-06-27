c90bbf3fc614045af1de509f886fc443f6c79167
20171105114 <13948948183@163. com>
2018-06-27 11:03:04 +0800

    Initial Commit

---
+++ 评委评分/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  评委评分
+//
+//  Created by lishuxin on 2018/6/27.
+//  Copyright © 2018年 lishuxin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
